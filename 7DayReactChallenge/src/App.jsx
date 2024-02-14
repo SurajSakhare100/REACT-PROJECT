6e0c3053f6f7a35087ad383f7c41c3faa8b51ed1
suraj sakhare <sakharesuraj10@gmail.com>
2024-02-14 20:58:43 +0530

    intail commit

---
+++ 7DayReactChallenge/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import SignUpLogin from './SignUpLogin.jsx'
+function App() {
+  const [count, setCount] = useState(0)
+return(
+  <>
+  <SignUpLogin/>
+  </>
+)
+}
+
+export default App
