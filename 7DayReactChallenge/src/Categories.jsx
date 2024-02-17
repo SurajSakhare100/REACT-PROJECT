c467dca48ea47d11ff19e86d365f56ccd2398d12
suraj sakhare <sakharesuraj10@gmail.com>
2024-02-17 21:40:17 +0530

    intial commit

---
+++ 7DayReactChallenge/src/Categories.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Categories() {
+  return (
+    <div className='h-screen flex flex-col w-100 items-center justify-center'>
+      <h1 className='text-center'>Categories</h1>
+    </div>
+  )
+}
+
+export default Categories
---
+++ 7DayReactChallenge/src/Contact.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Contact() {
+  return (
+    <div className='h-screen flex flex-col w-100 items-center justify-center'>
+      <h1 className='text-center'>Contact</h1>
+    </div>
+  )
+}
+
+export default Contact
