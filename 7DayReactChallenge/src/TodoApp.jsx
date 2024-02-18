b6e380e52c68e2813ea15a4cf3b2f57b49131020
suraj sakhare <sakharesuraj10@gmail.com>
2024-02-18 21:10:28 +0530

    todo app

--- 7DayReactChallenge/src/TodoApp.jsx
+++ 7DayReactChallenge/src/TodoApp.jsx
@@ -14,12 +14,12 @@ function TodoApp() {
         </div>
         <div>
             <div class="flex mb-4 items-center">
-                <p class="w-full text-grey-darkest">Add another component to Tailwind Components</p>
+                <p class="w-full text-grey-darkest">Add Taks</p>
                 <button class="flex-no-shrink p-2 ml-4 mr-2 border-2 rounded hover:text-white text-green border-green hover:bg-green">Done</button>
                 <button class="flex-no-shrink p-2 ml-2 border-2 rounded text-red border-red hover:text-white hover:bg-red">Remove</button>
             </div>
           	<div class="flex mb-4 items-center">
-                <p class="w-full line-through text-green">Submit Todo App Component to Tailwind Components</p>
+                <p class="w-full line-through text-green">Submit Task</p>
                 <button class="flex-no-shrink p-2 ml-4 mr-2 border-2 rounded hover:text-white text-grey border-grey hover:bg-grey">Not Done</button>
                 <button class="flex-no-shrink p-2 ml-2 border-2 rounded text-red border-red hover:text-white hover:bg-red">Remove</button>
             </div>
