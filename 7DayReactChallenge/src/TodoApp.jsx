ee3c82576bd2a17b0047f150d0938448e155896c
suraj sakhare <sakharesuraj10@gmail.com>
2024-02-20 21:14:26 +0530

    basic change

--- 7DayReactChallenge/src/TodoApp.jsx
+++ 7DayReactChallenge/src/TodoApp.jsx
@@ -10,7 +10,11 @@ function TodoApp() {
             
         </div>
         <div>
-           ffs
+            <div class="flex mb-4 items-center">
+                <p class="w-full text-grey-darkest">Add Taks</p>
+                <button class="flex-no-shrink p-2 ml-4 mr-2 border-2 rounded hover:text-white text-green border-green hover:bg-green">Done</button>
+                <button class="flex-no-shrink p-2 ml-2 border-2 rounded text-red border-red hover:text-white hover:bg-red">Remove</button>
+            </div>
           	<div class="flex mb-4 items-center">
                 <p class="w-full line-through text-green">Submit Task</p>
                 <button class="flex-no-shrink p-2 ml-4 mr-2 border-2 rounded hover:text-white text-grey border-grey hover:bg-grey">Not Done</button>
