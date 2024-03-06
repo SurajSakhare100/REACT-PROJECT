9d9b223a54b70286a3dc521577e188790984be46
suraj sakhare <sakharesuraj10@gmail.com>
2024-03-06 21:25:21 +0530

    todo app

--- React Project/src/Component/TodoItem.jsx
+++ React Project/src/Component/TodoItem.jsx
@@ -1,17 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { TodoContext, usetodo } from '../Api/TodoContext'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCableCar, faDumpster, faPencil, faXmark } from '@fortawesome/free-solid-svg-icons'
 function TodoItem() {
-    const todoitem = usetodo(TodoContext)
+    const { todos, deleteTodo, addTodo, updateTodo,toggleComplete } = usetodo()
+    const [editable,seteditable]=useState(true)
+    const [completed,setcompleted]=useState(false)
+    function editTodo() {
+        updateTodo(todos.id,{...todos,todos})
+        seteditable(false)
+    }
     return (
-        <div className='flex gap-4 w-full text-black'>
-
-            <div>
-                {todoitem.map((todo,id) => {
-                    <div key={id}>
-                        <p className='text-black'>{todo.id}</p>
-                        <p className='text-black'>{todo.title}</p>
+        <div className='flex gap-4 w-1/2 m-auto text-black'>
+            <div className='w-full h-full rounded-lg my-2 '>
+                {todos.map((todo, id) => (
+                    <div key={id} className={`w-full my-2 py-2 px-4 ${todo.completed?'bg-slate-400':'bg-pink-200'} rounded-xl flex items-center justify-between`}>
+                        <div className='flex gap-2 items-center'>
+                            <input 
+                            type="checkbox"
+                            className='cursor-pointer'
+                            checked={todo.completed} 
+                            onChange={toggleComplete(todo.id)}/>
+                            <input 
+                            className={`text-black text-md bg-transparent outline-none border-none ${todo.completed?'line-through':''}`} 
+                            readOnly={editable}
+                            value={todo}
+                             />
+                        </div>
+                        <div className='flex items-center gap-2  '>
+                            <div onClick={() => { editTodo() }} className={`w-[40px] h-[40px] cursor-pointer rounded-lg ${editable?'bg-white':'bg-[#ffffffa2]'} flex items-center justify-center`}>
+                                {/* <FontAwesomeIcon icon={faPencil} /> */}
+                                <p className={`text-[18px] }`} onClick={()=>{seteditable((a)=>!a)}}>✏️</p>
+                            </div>
+                            <div onClick={() => { deleteTodo(todo.id) }} className='w-[40px] cursor-pointer h-[40px] rounded-lg bg-white flex items-center justify-center'>
+                                <FontAwesomeIcon icon={faXmark} className='text-red-600 text-2xl'/>
+                            </div>
+                        </div>
                     </div>
-                })}
+                ))}
             </div>
         </div>
     )
--- React Project/src/pages/Todo.jsx
+++ React Project/src/pages/Todo.jsx
@@ -5,27 +5,44 @@ import TodoItem from '../Component/TodoItem';
 function Todo() {
     const todoinput = useRef(null);
     const [todos, setTodo] = useState([]);
+    console.log(todos)
     const addTodo=(todo) => {
         setTodo((prev)=>[{id:Date.now(),...todo},...prev])
      }
-    const updateTodo=(todo,id) => {
+    const updateTodo=(id) => {
         (prev)=>prev.map((prevTodo)=>prevTodo.id==id?todo:prevTodo)
      }
     const deleteTodo=(id)=>{
-        setTodo((prev)=>prev.filter((todo)=>todo.id!==id))
+        console.log(id)
+        setTodo((prev)=>prev.filter((todo)=>{return todo.id!=id}))
     }
     const toggleComplete=(id)=>{
-        (prev)=>prev.map((prevTodo)=>prevTodo.id==id?{...prevTodo,completed:!prevTodo.completed}:prevTodo)
+        
+        setTodo((prev)=>prev.map((prevTodo)=>prevTodo.id==id?{...prevTodo,completed:!prevTodo.completed}:prevTodo))
     }
+    useEffect(() => {
+        // const todost=JSON.parse(localStorage.getItem("todos"))
+        // if(todos && todos.length>0){
+        //     setTodo(todost)
+        // }
+    }, [])
+    useEffect(() => {
+        // localStorage.setItem('todos',JSON.stringify(todos))
+    }, [setTodo])
+    
     return (
         <>
-            <TodoContextProvider value={todos}>
-                <h2 className='w-100 text-center text-2xl '>Todo App</h2>
-                <div className='flex gap-4 justify-center items-center'>
-                    <input ref={todoinput} type="text" placeholder='Add todo' className='border-2 px-2 py-1 w-1/2 border-black rounded-lg' />
-                    <button onClick={() => {addTodo({id:Date.now(),title:todoinput.current.value,completed:true})}} className='bg-green-300 px-4 py-1 rounded-lg'>Add Todo</button>
-                </div>
+            <TodoContextProvider value={{todos,deleteTodo,toggleComplete,addTodo,updateTodo}}>
+                <div className='bg-[#111943] w-full h-screen '>
+                <h2 className='w-100 text-white text-center text-4xl font-bold mb-2 py-2'>Todo App</h2>
+                <div className='flex gap-4 justify-center items-center w-full flex-col'>
+                    <div className='w-1/2 m-auto flex gap-2 justify-center'>
+                    <input ref={todoinput} type="text" placeholder='Add todo' className='border-2 text-white outline-none px-2 w-4/5 py-2 border-none bg-[#ffffff39] rounded-lg' />
+                    <button onClick={() => {addTodo({id:Date.now(),title:todoinput.current.value,completed:true})}} className='bg-green-400 px-4 w-1/5  py-2 rounded-lg'>Add Todo</button>
+                    </div>
                 <TodoItem />
+                </div>
+                </div>
             </TodoContextProvider>
         </>
     )
